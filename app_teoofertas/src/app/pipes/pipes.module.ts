f7b99f28950c24ae0e072abbef9493e83b1df102
Lucas Zanotelli <lucas.za.notelli@hotmail.com>
2019-09-28 20:32:47 -0300

    primeiro

--- app_teoofertas/src/app/pipes/pipes.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { FiltroPipe } from './filtro.pipe';
-
-@NgModule({
-  declarations: [FiltroPipe],
-  exports: [FiltroPipe]
-})
-export class PipesModule { }
